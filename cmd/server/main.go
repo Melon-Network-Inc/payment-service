7c182b4c0b8dfd27c53bb276606042efa03bdac2
Michael Zhou <michaelzhou@melonnetwork.io>
2022-08-21 01:37:12 -0700

    [Refactor] Move payment service to port 7001 to fix issue running on latest MacOS.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,7 +7,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Melon-Network-Inc/account-service/pkg/friend"
 	"github.com/Melon-Network-Inc/account-service/pkg/user"
@@ -25,9 +29,11 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// ServiceName indicates the name of current service.
 // Version indicates the current version of the application.
-var ServiceName = "payment-service"
-var Version = "1.0.0"
+const ServiceName = "payment-service"
+const Version = "1.0.0"
+
 var swagHandler gin.HandlerFunc
 
 func init() {
@@ -35,33 +41,66 @@ func init() {
 }
 
 type Server struct {
-	App  			*gin.Engine
-	Cache  			*dbcontext.Cache
-	Database 		*dbcontext.DB
-	Logger          log.Logger
+	App      *gin.Engine
+	Cache    *dbcontext.Cache
+	Database *dbcontext.DB
+	Logger   log.Logger
 }
 
 func main() {
-	config := config.BuildServerConfig(ServiceName, Version)
+	serverConfig := config.BuildServerConfig(ServiceName, Version)
 
 	// create root logger tagged with server version
-	logger := log.New(config.ServiceName).With(context.Background(), "version", config.Version)
-	
-	db, err := dbcontext.ConnectToDatabase(config.DatabaseUrl)
+	logger := log.New(serverConfig.ServiceName).With(context.Background(), "version", serverConfig.Version)
+
+	db, err := dbcontext.ConnectToDatabase(serverConfig.DatabaseUrl)
 	if err != nil {
 		logger.Error(err)
 		os.Exit(-1)
 	}
-	
+
 	s := Server{
-		App : gin.Default(), 
-		Cache: dbcontext.NewCache(dbcontext.ConnectToCache(config.CacheUrl)), 
-		Database: dbcontext.NewDatabase(db), 
-		Logger: logger,
+		App:      gin.Default(),
+		Cache:    dbcontext.NewCache(dbcontext.ConnectToCache(serverConfig.CacheUrl)),
+		Database: dbcontext.NewDatabase(db),
+		Logger:   logger,
 	}
 
 	s.buildHandlers()
-	s.App.Run(fmt.Sprintf(":%d", config.ServerPort))
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", serverConfig.ServerPort),
+		Handler: s.App,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Errorf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be caught, so don't need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Errorf("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		logger.Info("timeout of 5 seconds.")
+	}
+	logger.Info("Server exiting")
 }
 
 func (s *Server) buildHandlers() {
@@ -80,12 +119,11 @@ func (s *Server) buildHandlers() {
 	}
 }
 
-func (s Server) buildSwagger() {
+func (s *Server) buildSwagger() {
 	docs.SwaggerInfo.Title = "Payment Service API"
 	docs.SwaggerInfo.Description = "This is payment server for Melon Wallet."
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:7000"
+	docs.SwaggerInfo.Host = "localhost:7001"
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 }
-
--- config/config.go
+++ config/config.go
@@ -3,36 +3,36 @@ package config
 import "github.com/spf13/viper"
 
 type ServiceConfig struct {
-	ServiceName		string
-	Version	   		string
-	ServerPort		int
-	DatabaseUrl  	string
-	CacheUrl    	string
-	EmailApiKey     string
+	ServiceName string
+	Version     string
+	ServerPort  int
+	DatabaseUrl string
+	CacheUrl    string
+	EmailApiKey string
 }
 
 func BuildServerConfig(ServiceName string, Version string) ServiceConfig {
 	sc := ServiceConfig{}
-	
+
 	viper.SetConfigFile("./pkg/envs/.env")
 
 	if err := viper.ReadInConfig(); err == nil {
 		sc = ServiceConfig{
 			ServiceName: ServiceName,
-			Version: 	 Version,
-			ServerPort: viper.Get("PORT").(int),
+			Version:     Version,
+			ServerPort:  viper.Get("PORT").(int),
 			DatabaseUrl: viper.Get("DB_URL").(string),
-			CacheUrl: viper.Get("CACHE_URL").(string),
+			CacheUrl:    viper.Get("CACHE_URL").(string),
 			EmailApiKey: viper.Get("SEND_GRID").(string),
 		}
 	} else {
 		sc = ServiceConfig{
-			ServiceName: 	ServiceName,
-			Version: 	 	Version,
-			ServerPort: 	7000,
-			DatabaseUrl: 	"postgres://postgres:postgres@localhost:5432/melon_service",
-			CacheUrl: 		"localhost:6379",
-			EmailApiKey: 	"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",
+			ServiceName: ServiceName,
+			Version:     Version,
+			ServerPort:  7001,
+			DatabaseUrl: "postgres://postgres:postgres@localhost:5432/melon_service",
+			CacheUrl:    "localhost:6379",
+			EmailApiKey: "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",
 		}
 	}
 	return sc
